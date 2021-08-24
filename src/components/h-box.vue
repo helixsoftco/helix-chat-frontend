13571d6c84c89222ff03bfd8846df7c7bf66c319
Cristian Diaz <titanmar15@gmail.com>
2021-08-24 14:23:10 -0500

    Interface design basis

--- src/components/h-box.vue
+++ src/components/h-box.vue
@@ -1,50 +1,38 @@
 <template>
   <div class="h-box">
-    <div class="h-box__input-container">
-      <h-input/>
+    <div class="h-chat" :class="isChatActive? 'active' : ''">
+      <div class="h-chat__header">
+        <div @click="showChat" class="h-icon-wrap">
+          <h-row-left />
+        </div>
+        <div class="h-user">Jonh Doe</div>
+      </div>
+      <h-box-body/>
+      <div class="h-chat__input-container">
+        <h-input/>
+      </div>
     </div>
+    <h-contact-list :class="!isChatActive? 'active' : ''" @showChat="showChat"/>
   </div>
 </template>
 
 <script>
 import HInput from "./h-input";
+import HBoxBody from "./h-box-body";
+import HRowLeft from "../icons/h-icon-arrow-left";
+import HContactList from "./h-contact-list";
 export default {
   name: "h-box",
-  components: {HInput}
+  components: {HContactList, HRowLeft, HBoxBody, HInput},
+  data() {
+    return {
+      isChatActive: false
+    }
+  },
+  methods: {
+    showChat() {
+      this.isChatActive = !this.isChatActive
+    }
+  },
 }
 </script>
-
-<style lang="scss">
-.h-box {
-  max-width: 0;
-  width: 100%;
-  height: 0;
-  box-shadow: 0 0 10px 0 rgba(0, 0, 0, .05);
-  border-radius: 10px;
-  position: fixed;
-  bottom: 70px;
-  right: 70px;
-  background-color: $h-bg-box;
-  transition: all ease-in-out .3s;
-  padding: 1rem;
-  overflow: hidden;
-
-  &__input-container {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 100px;
-    display: flex;
-    align-items: center;
-    padding: 1rem;
-    border-top: 1px solid #404e81;
-  }
-
-  &.active {
-    height: 600px;
-    max-width: 360px;
-  }
-}
-
-</style>
